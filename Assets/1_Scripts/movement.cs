c4855fb3f338d9cdf9c82c0557e22ff85f97c68d
DBZDokkanBattle <barthpaul224@gmail.com>
2025-09-05 11:37:15 +0200

    move 2

--- Assets/1_Scripts/movement.cs
+++ Assets/1_Scripts/movement.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField]
     public float speed = 10;
-    
+    private int direction;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) { direction = 1; }
+        if (Input.GetKey(KeyCode.RightArrow)) { direction = 2; }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        direction = 0;
+    }
+
+
     public void Update()
     {
         
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && direction != 1)
         {
             transform.Translate(Vector3.left * (Time.deltaTime * speed));
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && direction != 2)
         {
             transform.Translate(Vector3.right * (Time.deltaTime * speed));
         }
